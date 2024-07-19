6eaefe307f0fc0d179846a355a15c0a30adfa6f2
Leonardo Paz <leonardocovelodapaz@gmail.com>
2024-07-18 21:59:59 -0300

    implementing exercise 1

---
+++ app/src/main/java/br/com/maschdy/kotlinfundamentals/Exercise1.kt
@@ -0,0 +1,15 @@
+package br.com.maschdy.kotlinfundamentals
+
+fun main() {
+    var mutableVariable = 6
+    mutableVariable = 5
+    println(mutableVariable)
+
+//    val immutableVariable = 6
+//    immutableVariable = 7
+//    println(immutableVariable)
+
+    /* Ao tentar alterar o valor de uma variável imutável,
+    ela retorna um erro na linha 9 ao rodar o código: Val cannot be reassigned
+    */
+}
